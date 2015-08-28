9b2d936c7927909268ec5e83b4a9681799a84992
Duran <duran.hsieh@afusion.com>
2015-08-28 18:30:07 +0800

    finish TaskModule(Autofac.Module)

--- Core.Common/Task/IRunOnEachRequest.cs
+++ Core.Common/Task/IRunOnEachRequest.cs
@@ -1,6 +1,19 @@
-﻿namespace Core.Common.Task
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Common.Task
 {
-    interface IRunOnEachRequest
+    /// <summary>
+    /// 在每一個Request執行之前，要執行的內容
+    /// </summary>
+    public interface IRunOnEachRequest
     {
+        /// <summary>
+        /// 要執行的邏輯
+        /// </summary>
+        void Execute();
     }
 }
---
+++ Core/AutoFacModule/TaskModule.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using Autofac;
+using Core.Common.Task;
+
+namespace Core.AutofacModule
+{
+    /// <summary>
+    /// Autofac用來註冊Task相關的服務
+    /// </summary>
+    public class TaskModule : Autofac.Module
+    {
+        /// <summary>
+        /// Override to add registrations to the container.
+        /// </summary>
+        /// <param name="builder">The builder through which components can be
+        /// registered.</param>
+        /// <remarks>
+        /// Note that the ContainerBuilder parameter is unique to this module.
+        /// </remarks>
+        protected override void Load(Autofac.ContainerBuilder builder)
+        {
+            var assemblies = Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assemblies).As<IRunAfterEachRequest>().InstancePerRequest();
+            builder.RegisterAssemblyTypes(assemblies).As<IRunAtStartup>();
+            builder.RegisterAssemblyTypes(assemblies).As<IRunOnEachRequest>().InstancePerRequest();
+            builder.RegisterAssemblyTypes(assemblies).As<IRunOnError>();
+        }
+    }
+}
