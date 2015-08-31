cf73791978dfdf61d5618201ee52b26a8fb3fc30
matsurigoto <dog0416@gmail.com>
2015-08-31 15:05:27 +0800

    add SearchViewmodelBase and reinstall pagedlist

---
+++ Core.Common/Base/ISearchFormViewModelBase.cs
@@ -0,0 +1,40 @@
+﻿namespace Core.Common.Base
+{
+    /// <summary>
+    /// 搜索 Form 的 ViewModel interface。定義搜索必須要有的相關欄位。
+    /// </summary>
+    public interface ISearchFormViewModelBase
+    {
+        /// <summary>
+        /// 取得或設定此搜索邏輯是要用Ascending還是Descending
+        /// </summary>
+        /// <value>
+        /// <c>true</c> 搜索邏輯是要用Ascending; 要不然, <c>false</c>.
+        /// </value>
+        bool IsAscending { get; set; }
+
+        /// <summary>
+        /// 取得或設定要依照那個排序的欄位
+        /// </summary>
+        /// <value>
+        /// 要依照那個排序的欄位
+        /// </value>
+        string OrderByColumnName { get; set; }
+
+        /// <summary>
+        /// 取得或設定目前頁數
+        /// </summary>
+        /// <value>
+        /// 目前頁數
+        /// </value>
+        int Page { get; set; }
+
+        /// <summary>
+        /// 取得或設定每頁幾筆
+        /// </summary>
+        /// <value>
+        /// 每頁幾筆
+        /// </value>
+        int PageSize { get; set; }
+    }
+}
---
+++ Core.Common/Base/ISearchViewModelBase.cs
@@ -0,0 +1,29 @@
+﻿namespace Core.Common.Base
+{
+    /// <summary>
+    /// 搜索頁面的ViewModel需要實作的一個interface。
+    /// 方便處理Paging和搜索條件相關。
+    /// 這個方法就兩個Property，用作於表示搜索的form和搜索結果的result。
+    /// </summary>
+    /// <typeparam name="TSearchForm">搜索的form ViewModel type。必須是繼承<see cref="MvcInfrastructure.Common.Base.SearchFormViewModelBase"/></typeparam>
+    /// <typeparam name="TPageResult">搜索的結果ViewModel type</typeparam>
+    public interface ISearchViewModelBase<TSearchForm, TPageResult>
+     where TSearchForm : Core.Common.Base.ISearchFormViewModelBase, new()
+    {
+        /// <summary>
+        /// 取得或設定搜索結果
+        /// </summary>
+        /// <value>
+        /// 搜索結果
+        /// </value>
+        PagedList.IPagedList<TPageResult> Result { get; set; }
+
+        /// <summary>
+        /// 取得或設定搜索的Form
+        /// </summary>
+        /// <value>
+        /// 搜索的Form
+        /// </value>
+        TSearchForm SearchForm { get; set; }
+    }
+}
