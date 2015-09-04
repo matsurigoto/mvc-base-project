dc2c0bdcf1be3e79d6faa9d35d6ff1ef817b2a60
matsurigoto <dog0416@gmail.com>
2015-09-04 17:11:52 +0800

    add OWIN setting(Startup.cs, Startup.Auth.cs)

---
+++ BaseProject.Admin/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace BaseProject.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BaseProject.Admin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BaseProject.Admin.Startup))]
+namespace BaseProject.Admin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
