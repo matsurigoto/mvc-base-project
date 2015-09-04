8655d256aef6af9d5ca0a05e9e66cb8388f1b540
matsurigoto <dog0416@gmail.com>
2015-09-04 17:24:29 +0800

    add BundleConfig.cs ,FilterConfig.cs , and install Microsoft.AspNet.Web.Optimization

---
+++ BaseProject.Admin/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BaseProject.Admin
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
